a75fe439c60cef9df4a01c281f3a363a6f27cb38
Nhadv <Daovannha6319981998@gmail.com>
2024-01-28 23:16:28 +0700

    viewwilldisapper

--- SectionTable/SectionTable/ViewController.swift
+++ SectionTable/SectionTable/ViewController.swift
@@ -22,7 +22,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
 
 }
 
